a22bb273ba41a6a783ff67d403ae30310fca3c47
MAtifJaved <atif_javed1992@yahoo.com>
2021-05-17 13:27:15 +0500

    files added

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Slide from 'react-reveal/Slide';
+import style from './nav.module.css';
+import Tilt from 'react-tilt';
+
+
+function Header(){
+   
+    return(
+    <>
+    <Tilt className="Tilt" options={{ max : 15 }}  >
+ <div className="Tilt-inner">  <div className={style.nav}>
+        <div className={style.navtext}>
+         <h1>
+         <Tilt className="Tilt" options={{ max : 15 }}  >
+ <div className="Tilt-inner">
+        <Slide left cascade>
+          ENCODERS
+        </Slide></div>
+      </Tilt>
+      </h1>
+    
+       
+      </div>
+      </div>
+  </div>
+</Tilt>       
+    </>
+    );
+
+}
+
+export default Header;
